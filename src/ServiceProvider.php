7de841622935bbef9de6a1684fcbd0b9f95642de
Philipp Daun <post@philippdaun.net>
2025-11-17 13:31:09 +0100

    Remove fieldtypes array
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,10 +6,6 @@ use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider
 {
-    protected $fieldtypes = [
-        Fieldtypes\IconGroup::class,
-    ];
-
     protected $vite = [
         'input' => [
             'resources/js/addon.js',
