a13c89f63e8ab9af107caf6bc4d8d494470d1aef
Philipp Daun <post@philippdaun.net>
2025-11-17 19:00:16 +0100

    Add icon toggles fieldtype
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- resources/js/addon.js
+++ resources/js/addon.js
@@ -1,3 +1,5 @@
 import IconGroupFieldtype from './components/IconGroupFieldtype.vue'
+import IconTogglesFieldtype from './components/IconTogglesFieldtype.vue'
 
 Statamic.$components.register('icon_group-fieldtype', IconGroupFieldtype)
+Statamic.$components.register('icon_toggles-fieldtype', IconTogglesFieldtype);
