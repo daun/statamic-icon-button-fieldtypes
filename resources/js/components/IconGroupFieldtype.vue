70d14286342542ab3eb40deb0ee136def6cedf8e
Philipp Daun <post@philippdaun.net>
2025-12-11 22:38:44 +0100

    Fix shadow extending beyond button group
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- resources/js/components/IconGroupFieldtype.vue
+++ resources/js/components/IconGroupFieldtype.vue
@@ -1,23 +1,25 @@
 <template>
-    <ui-button-group ref="buttonGroup">
-        <ui-button
-            v-for="(option, $index) in options"
-            ref="button"
-            :disabled="config.disabled"
-            :key="$index"
-            :name="name"
-            :read-only="isReadOnly"
-            :icon-only="true"
-            :value="option.value"
-            :variant="value == option.value ? 'pressed' : 'default'"
-            :aria-label="option.label || option.value"
-            :size="config.size || 'base'"
-            v-tooltip="option.label || option.value"
-            @click="updateSelectedOption(option.value)"
-        >
-            <ui-icon :name="option.icon" :set="config.set ?? 'default'" />
-        </ui-button>
-    </ui-button-group>
+    <div class="flex">
+        <ui-button-group ref="buttonGroup">
+            <ui-button
+                v-for="(option, $index) in options"
+                ref="button"
+                :disabled="config.disabled"
+                :key="$index"
+                :name="name"
+                :read-only="isReadOnly"
+                :icon-only="true"
+                :value="option.value"
+                :variant="value == option.value ? 'pressed' : 'default'"
+                :aria-label="option.label || option.value"
+                :size="config.size || 'base'"
+                v-tooltip="option.label || option.value"
+                @click="updateSelectedOption(option.value)"
+            >
+                <ui-icon :name="option.icon" :set="config.set ?? 'default'" />
+            </ui-button>
+        </ui-button-group>
+    </div>
 </template>
 
 <script>
